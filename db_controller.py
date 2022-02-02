54b1dc9320c7a4ef17e02dc58f784e0d3ef3197b
Mariia Romaniuk <romanuik7meri.ua@gmail.com>
2022-02-02 18:28:42 +0200

    add try except to get_db_creds function

--- db_controller.py
+++ db_controller.py
@@ -13,26 +13,30 @@ def create_table(name):
 
 
 def get_db_creds(path):
-    data = []
-    with open(path, "r") as myfile:
-        data = myfile.readlines()
+    try:
+        with open(path, "r") as myfile:
+            data = myfile.readlines()
+    except Exception as e:
+        raise e
+    else:
+        var_dict = {}
+        for var in data:
+            var = var.replace('\n', '').split('=')
+            var_dict[var[0]] = var[1]
 
-    var_dict = {}
-    for var in data:
-        var = var.replace('\n', '').split('=')
-        var_dict[var[0]] = var[1]
-
-    return var_dict
+        return var_dict
 
 
 def connect_to_db():
-    creds = get_db_creds("./db_credentials.txt")
+    # creds = get_db_creds("./db_credentials.txt")
+
+    credentials = get_db_creds(".../db_credentials.txt")
     connection = psycopg2.connect(
-        host=creds["host"],
-        port=creds["port"],
-        user=creds["user"],
-        password=creds["password"],
-        database=creds["database"]
+        host=credentials["host"],
+        port=credentials["port"],
+        user=credentials["user"],
+        password=credentials["password"],
+        database=credentials["database"]
     )
     return connection
 
