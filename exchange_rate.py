2cb39c143ab9a74dc22435e3619a1b74613f2cce
Mariia Romaniuk <romanuik7meri.ua@gmail.com>
2022-02-04 16:31:00 +0200

    exchange_rate_analysis: get data from database

--- exchange_rate.py
+++ exchange_rate.py
@@ -6,6 +6,8 @@ import requests
 import moment
 import pandas as pd
 
+from db_controller import connect_to_db, get_data_from_db
+
 
 def dates_between_two_dates(startDate, endDate):
     diff = abs(startDate.diff(endDate).days)
@@ -31,7 +33,6 @@ def get_list_of_data(data):
     return list_of_date, list_of_rate
 
 
-
 def most_frequent_rates(df):
     rates_frequency = {}
     for index, row in df.iterrows():
@@ -41,7 +42,9 @@ def most_frequent_rates(df):
             rates_frequency[str(row['rate'])] = 1
 
     sorted_frequency = {k: v for k, v in sorted(rates_frequency.items(), key=lambda item: item[1], reverse=True)}
-    return {k: sorted_frequency[k] for k in list(sorted_frequency)[:5]}
+    rates_dict = {k: sorted_frequency[k] for k in list(sorted_frequency)[:5]}
+    for key, value in zip(rates_dict.keys(), rates_dict.values()):
+        print(f"{key} - {value} times")
 
 
 def exchange_rate_analysis(df):
@@ -59,22 +62,15 @@ def exchange_rate_analysis(df):
 
     month_list = []
     for i in range(0, len(df)):
-        month = get_list_of_data(response)[0][i][3:5]
+        month = df["date"][i][3:5]
         month_list.append(month)
-
-    data_month_dict = tuple(zip(month_list, get_list_of_data(response)[1]))
-    data_month_list = list(data_month_dict)
-    df_month = pd.DataFrame(data_month_list, columns=["month", "rate"])
-    grouped_data = df_month.groupby(["rate"]).mean()
-    print(grouped_data)
-
-    favorable_year_rate = grouped_data["rate"].min()
-    print("Favorable year rate", favorable_year_rate)
-    # favorable_month = grouped_data[grouped_data["rate"] == favorable_year_rate]
-    # print("The most favorable average monthly rate for the year was:", favorable_month, favorable_year_rate)
-
-    most_frequent = most_frequent_rates(df)
-    print(most_frequent)
+    df["month_number"] = month_list
+    grouped_data = df.groupby(["month_number"]).mean()
+    favorable_year_rate = grouped_data.nsmallest(1, "rate")
+    # print("Favorable year rate", favorable_year_rate["rate"])
+    print("Favorable year rate:", favorable_year_rate["rate"][0])
+    print("Most frequent rates: ")
+    most_frequent_rates(df)
 
 
 def dataframe_to_csv(csv_name, dictionary):
@@ -82,8 +78,7 @@ def dataframe_to_csv(csv_name, dictionary):
     if not os.path.exists(os.path.dirname(csv_name)):
         try:
             os.makedirs(os.path.dirname(csv_name))
-            # os.mkdir(os.path.dirname(f'.{csv_name}'))
-        except OSError as exc:  # Guard against race condition
+        except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise
     csv_file = df.to_csv(csv_name, encoding='utf-8')
@@ -92,23 +87,26 @@ def dataframe_to_csv(csv_name, dictionary):
 
 
 if __name__ == "__main__":
-    start_date = moment.date('2021-01-01')
-    end_date = moment.date('2022-01-01')
-    dates = list(dates_between_two_dates(start_date, end_date))
-    list_of_url = list(map(get_url, dates))
-    response = list(map(lambda url: requests.get(url).json(), list_of_url))
-    data_dict = {"date": get_list_of_data(response)[0], "rate": get_list_of_data(response)[1]}
-    csv_file_name = "./data/currency.csv"
-    dataframe_to_csv(csv_file_name, data_dict)
+    # start_date = moment.date('2021-01-01')
+    # end_date = moment.date('2022-01-01')
+    # dates = list(dates_between_two_dates(start_date, end_date))
+    # list_of_url = list(map(get_url, dates))
+    # response = list(map(lambda url: requests.get(url).json(), list_of_url))
+    # data_dict = {"date": get_list_of_data(response)[0], "rate": get_list_of_data(response)[1]}
+    # path_to_csv_file = "./data/currency.csv"
+    # dataframe_to_csv(path_to_csv_file, data_dict)
 
+    file_name = "currency.csv"
+    csv_file_name = f'data/{file_name}'
+    bucket_name = 's3-all-data'
 
+    conn = connect_to_db()
+    conn.autocommit = True
+    cursor = conn.cursor()
+    # print(create_table("CURRENCY_RATE"), cursor)
+    # print(from_s3_to_postgres(bucket_name, file_name, cursor, conn))
+    dataframe = get_data_from_db(cursor, conn)
+    conn.close()
 
-    # dict_to_csv(data_dict, file_name, columns)
-    # dataframe = pd.DataFrame(data_items, columns=["data", "rate"])
-    # csv_data = to_csv(dataframe, "currency")
-    # exchange_rate_analysis(dataframe)
+    exchange_rate_analysis(dataframe)
 
-    # Question: df to csv or  csv to df?
-    # l did df to csv, but l need to write from csv to database then read tas dataframe
-
-    # json to csv,  csv to s3, s3 to postrgres,  postrger read as dataframe
